f0adc3a22d57b1c4876a14f3525f243e0756ce6d
mansigrapes <mansi.patel@grapes-solutions.com>
2022-05-02 11:30:55 +0530

    Session_2 All Practical

---
+++ Mansi/S2_P2.py
@@ -0,0 +1,9 @@
+# Create four variables with different type and print its data type
+a=10
+b="Mansi"
+c=5.5
+d=23
+print("Value of a:",a,"DataType of a :",type(a))
+print("Value of b:",b,"DataType of b :",type(b))
+print("Value of c:",c,"DataType of c :",type(c))
+print("Value of d:",a,"DataType of d :",type(d))
---
+++ Mansi/S2_P5.py
@@ -0,0 +1,3 @@
+ #Create tuple with 10 items & print it
+city=("Ahmedabad","Baroda","Ghandhinagar","Mehsana","Unjha","Patan","Surat","Nadiad","Aanad","Rajkot")
+print(city)
