43d01e9d2154f092de464ab1c0a72e36f890b680
Mansi Patel <mansi.patel@grapes-solutions.com>
2022-05-03 14:45:48 +0530

    Update S2_P2.py

--- Mansi/S2_P2.py
+++ Mansi/S2_P2.py
@@ -2,7 +2,7 @@
 a=10
 b="Mansi"
 c=5.5
-d=23
+d={"Mansi","Ishan","Zeel"}
 print("Value of a:",a,"DataType of a :",type(a))
 print("Value of b:",b,"DataType of b :",type(b))
 print("Value of c:",c,"DataType of c :",type(c))
