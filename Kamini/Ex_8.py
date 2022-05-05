d39c7f65a5aed1fc6d8a88cb42a92d202bd706b0
kaminigrapes <kamini.dabasiya@grapes-solutions.com>
2022-05-05 15:04:22 +0530

    practical

---
+++ Kamini/Ex_8.py
@@ -0,0 +1,7 @@
+# Create dictionary with any items and print all items
+
+dictionary = {
+    'name':'Kamini',
+    'email':'kamini.dabasiya@grapes-solutions.com'
+}
+print("My saved dictionary:"+str(dictionary))
