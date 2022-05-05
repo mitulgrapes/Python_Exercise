c3e541f4410b7840bf844645ffd7ece3ab491bb6
vivekgrapes <vivek.acharya@grapes-solutions.com>
2022-05-06 01:15:37 +0530

    Python session2 exercise

---
+++ Vivek/session-2/exercise1.py
@@ -0,0 +1,8 @@
+#1) Store your personal information in variables and print it. (Fields: Firstname, Lastname, Email, Contact No, Age, Height)
+Firstname = "Vivek"
+Lastname = "Acharya"
+Email = "vivek.acharya@grapes-solutions.com"
+Contact_No = "9510172375"
+Age = "36"
+Height = "5.10"
+print("Firstname: "+Firstname+"\nLastname: "+Lastname+"\nEmail: "+Email+"\nContact No: "+Contact_No+"\nAge: "+Age+"\nHeight: "+Height)
---
+++ Vivek/session-2/exercise8.py
@@ -0,0 +1,10 @@
+# 8) Create dictionary with any items and print all values
+profile = {
+	'name':'Vivek',
+	'email':'vivek.acharya@grapes-solutions.com',
+	'mobile':'9510172375'
+}
+print('Name: '+profile['name'])
+print('Email: '+profile['email'])
+print('Mobile: '+profile['mobile'])
+print(len(profile))
