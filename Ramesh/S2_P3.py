b4107d8c1de167cf7de53acbde5dc63a835d4b7e
Ramesh Nagda <ramesh.nagda@papasiddhi.com>
2022-05-05 22:02:01 +0530

    Python Excercise by ramesh

---
+++ Ramesh/S2_P3.py
@@ -0,0 +1,3 @@
+#Declare list with at least 10 items and print items from 3rd position to 7th position
+list=["a","b","c","d","e","f","g","h","i","j"]
+print("Result - ",list[3:7])
---
+++ Ramesh/S2_P8.py
@@ -0,0 +1,8 @@
+#Create dictionary with any items and print all values
+dict = {
+		'a' : 'This',
+		'b' : 'Is',
+		'c' : 'Python',
+		'd' : 'Excercise',
+}
+print(dict.values())
