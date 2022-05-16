841047e8e72e7e2e02a2e7bd6e1f850d048088db
Mansi Patel <mansi.patel@grapes-solutions.com>
2022-05-16 10:52:41 +0530

    Delete S2_P3.py

--- Mansi/S2_P3.py
+++
@@ -1,5 +0,0 @@
-#Create list with at least 5 items. Print second & last second item value
-list=["First","Second","Third","Forth","Fifth"]
-print("Whole list",list)
-print("Second value - ",list[1])
-print("Last Second",list[-2])
