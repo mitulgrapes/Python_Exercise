c193016adbbbaf0054da33cd5cdbe4508af25f6b
Mansi Patel <mansi.patel@grapes-solutions.com>
2022-05-16 10:53:24 +0530

    Delete S2_P8.py

--- Mansi/S2_P8.py
+++
@@ -1,8 +0,0 @@
-#Create dictionary with any items and print all keys
-Informaion = {
-		'Name' : 'Mansi',
-		'Gender' : 'Female',
-		'Age' : '24 year',
-		'Nationality':'Indian',
-}
-print(Informaion.keys())
