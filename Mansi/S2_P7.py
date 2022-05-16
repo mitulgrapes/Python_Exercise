7e800608e69fcebdab40687499aa6916db422954
Mansi Patel <mansi.patel@grapes-solutions.com>
2022-05-16 10:53:12 +0530

    Delete S2_P7.py

--- Mansi/S2_P7.py
+++
@@ -1,7 +0,0 @@
-#Create set with 3 items and remove & add any item
-Laptop = {"Lenovo","Dell","HP"}
-print("Set :- ",Laptop)
-
-Laptop.remove("HP")
-Laptop.add("Apple")
-print("updated set :",Laptop)
