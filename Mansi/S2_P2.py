57d81e1e706f4b23e0c3302d44e53034a11c630e
Mansi Patel <mansi.patel@grapes-solutions.com>
2022-05-16 10:52:34 +0530

    Delete S2_P2.py

--- Mansi/S2_P2.py
+++
@@ -1,9 +0,0 @@
-# Create four variables with different type and print its data type
-a=10
-b="Mansi"
-c=5.5
-d={"Mansi","Ishan","Zeel"}
-print("Value of a:",a,"DataType of a :",type(a))
-print("Value of b:",b,"DataType of b :",type(b))
-print("Value of c:",c,"DataType of c :",type(c))
-print("Value of d:",a,"DataType of d :",type(d))
